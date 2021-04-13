b2625d50b203431e34011600ed88fec058277e6b
Tyler Hackett <ins10102@gmail.com>
2021-04-13 15:05:13 -0700

    Simple CUDA matrix multiply working for square matrix and vector input

--- classifier.cu
+++ classifier.cu
@@ -17,7 +17,7 @@ using namespace std;
 #endif
 
 /* The weights of the layer*/
-__device__ float weights[Ni][Nn];
+__device__ float d_weights[Ni*Nn];
 
 /*
  * Matrix multiply. Performs the operation c = ab', where b' is the transpose of b.
@@ -29,10 +29,21 @@ __device__ void mmult(float *a, float *b, float *c, int2 adim, int2 bdim)
 
     if(row < bdim.x && col < adim.y)
     {
-		for(int i = 0; i < adim.y; i++)
+		for(int i = 0; i < adim.x; i++)
 		{
-			c[col*bdim.x + row] += a[col*adim.x + i] * b[row*bdim.y + i];
+			c[row*bdim.x + col] += a[i*adim.y + col] * b[row*adim.y + i];
 		}
+    }
+}
+
+__device__ void relu(float *mtx, int2 dim)
+{
+    int row = threadIdx.x + blockIdx.x*blockDim.x;
+    int col = threadIdx.y + blockIdx.y*blockDim.y;
+
+    if(row < dim.x && col < dim.y)
+    {
+			mtx[col*dim.y + row] *= (mtx[col*dim.y + row] > 0);
     }
 }
 
@@ -41,8 +52,17 @@ __global__ void classify(float *in, float *out)
     const int2 in_dim = make_int2(Ni, 1);
     const int2 weight_dim = make_int2(Ni, Nn);
 
-    mmult((float*)weights, in, out, weight_dim, in_dim);
-} 
+    mmult(d_weights, in, out, weight_dim, in_dim);
+    //relu(out, make_int2(weight_dim.y, in_dim.x));
+}
+
+void randomizeArray(float *data, int len)
+{
+	for(int i = 0; i < len; i++)
+	{
+		data[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) * 16.0f;
+	}
+}
 
 int main(int argc, char **argv) 
 {
@@ -58,6 +78,15 @@ int main(int argc, char **argv)
      float h_out_data[Nn]; // Output on device
      float *d_out_data;    // Output on host
 
+     float *h_random_weights = new float[Ni*Nn];
+
+     // Make some random data.
+
+     randomizeArray(h_random_weights, Ni*Nn);
+     randomizeArray(h_in_data, Ni);
+
+     cudaMemcpyToSymbol(d_weights, h_random_weights, Nn*Ni*sizeof(float));
+
      cudaMalloc(&d_in_data, in_size);
      cudaMemcpy(d_in_data, h_in_data, in_size, cudaMemcpyHostToDevice); // Give the GPU our input data.
 
@@ -69,9 +98,18 @@ int main(int argc, char **argv)
 
      cudaMemcpy(h_out_data, d_out_data, out_size, cudaMemcpyDeviceToHost); // Retrieve the neuron outputs.
 
+
+     for(int i = 0; i < Nn*Ni; i++)
+		 printf("%f ", h_random_weights[i]);
+     printf("\n\n");
+
+     for(int i = 0; i < Nn; i++)
+    	 printf("%f ", h_in_data[i]);
+     printf("\n\n");
+
      for(int i = 0; i < Nn; i++)
     	 printf("%f ", h_out_data[i]);
-     printf("\n");
+     printf("\n\n");
 
     return 0;
 }
